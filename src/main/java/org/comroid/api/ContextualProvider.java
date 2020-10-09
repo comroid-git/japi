300888ea0c5e3696b2e3634cdde65511e4584c82
Tobias Burdow [Kaleidox] <burdoto@outlook.com>
2020-10-09 03:41:06 +0200

    Refined Context structure

--- src/main/java/org/comroid/api/ContextualProvider.java
+++ src/main/java/org/comroid/api/ContextualProvider.java
@@ -1,42 +1,49 @@
 package org.comroid.api;
 
 import org.jetbrains.annotations.ApiStatus.Experimental;
+import org.jetbrains.annotations.ApiStatus.NonExtendable;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Optional;
 import java.util.stream.StreamSupport;
 
 @Experimental
 public interface ContextualProvider {
     Iterable<Object> getContextMembers();
 
-    static ContextualProvider combine(ContextualProvider... providers) {
-        final List<Object> underlying = Arrays.asList(providers);
+    static ContextualProvider create(Object... members) {
+        final List<Object> underlying = Arrays.asList(members);
         return () -> underlying;
     }
 
-    default <T> Optional<T> getFromContext(final Class<T> memberType) {
+    @NonExtendable
+    default <T> Rewrapper<T> getFromContext(final Class<T> memberType) {
         return StreamSupport.stream(getContextMembers().spliterator(), false)
                 .map(member -> member instanceof ContextualTypeProvider
-                        && memberType.isAssignableFrom(((ContextualTypeProvider<?>) member).getContextMemberType())
-                        ? ((ContextualTypeProvider<?>) member).getFromContext()
+                        ?
+                        (memberType.isAssignableFrom(((ContextualTypeProvider<?>) member).getContextMemberType())
+                                ? ((ContextualTypeProvider<?>) member).getFromContext()
+                                : ((ContextualTypeProvider<?>) member).getFromContext(memberType).get())
                         : member)
                 .filter(memberType::isInstance)
                 .findAny()
-                .map(memberType::cast);
+                .map(memberType::cast)
+                .map(it -> (Rewrapper<T>) () -> it)
+                .orElse(() -> null);
     }
 
+    @NonExtendable
     default <T> @NotNull T requireFromContext(final Class<T> memberType) throws NoSuchElementException {
-        return getFromContext(memberType).orElseThrow(() -> new NoSuchElementException(String.format("No member of type %s found", memberType)));
+        return getFromContext(memberType).requireNonNull(() -> String.format("No member of type %s found", memberType));
     }
 
     interface Underlying extends ContextualProvider {
         ContextualProvider getUnderlyingContextualProvider();
 
         @Override
+        @NonExtendable
         default Iterable<Object> getContextMembers() {
             return getUnderlyingContextualProvider().getContextMembers();
         }
--- src/main/java/org/comroid/api/ContextualTypeProvider.java
+++ src/main/java/org/comroid/api/ContextualTypeProvider.java
@@ -1,39 +1,43 @@
 package org.comroid.api;
 
 import org.jetbrains.annotations.ApiStatus.Experimental;
+import org.jetbrains.annotations.ApiStatus.NonExtendable;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collections;
-import java.util.Optional;
 
 @Experimental
 public interface ContextualTypeProvider<T> extends ContextualProvider {
     @NotNull T getFromContext();
 
+    @NonExtendable
     default Class<T> getContextMemberType() {
         return Polyfill.uncheckedCast(getFromContext().getClass());
     }
 
     @Override
+    @NonExtendable
     default Iterable<Object> getContextMembers() {
         return Collections.singleton(getFromContext());
     }
 
     @Override
-    default <R> Optional<R> getFromContext(final Class<R> memberType) {
+    @NonExtendable
+    default <R> Rewrapper<R> getFromContext(final Class<R> memberType) {
         return memberType.isAssignableFrom(getContextMemberType())
-                ? Optional.of(memberType.cast(getFromContext()))
-                : Optional.empty();
+                ? () -> memberType.cast(getFromContext())
+                : () -> null;
     }
 
     interface This<T> extends ContextualTypeProvider<T> {
         @Override
-        @NotNull
-        default T getFromContext() {
+        @NonExtendable
+        default @NotNull T getFromContext() {
             return Polyfill.uncheckedCast(this);
         }
 
         @Override
+        @NonExtendable
         default Class<T> getContextMemberType() {
             return Polyfill.uncheckedCast(getClass());
         }
@@ -41,13 +45,14 @@ public interface ContextualTypeProvider<T> extends ContextualProvider {
 
     interface Underlying<T> extends ContextualTypeProvider<T>, ContextualProvider.Underlying {
         @Override
+        @NonExtendable
         default Iterable<Object> getContextMembers() {
             return getUnderlyingContextualProvider().getContextMembers();
         }
 
         @Override
-        @NotNull
-        default T getFromContext() {
+        @NonExtendable
+        default @NotNull T getFromContext() {
             return getUnderlyingContextualProvider().getFromContext();
         }
 
