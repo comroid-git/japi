c613fe008aef84ac3183bb0cf206ef3613a1b092
Tobias Burdow [Kaleidox] <burdoto@outlook.com>
2021-01-09 19:59:00 +0100

    Create GenericNotation.java

---
+++ src/main/java/org/comroid/util/GenericNotation.java
@@ -0,0 +1,26 @@
+package org.comroid.util;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+
+// shamelessly stolen, but unused
+public abstract class GenericNotation<T> implements Comparable<GenericNotation<T>> {
+    protected final Type type;
+
+    protected GenericNotation() {
+        Type superClass = getClass().getGenericSuperclass();
+        if (superClass instanceof Class<?>) {
+            throw new IllegalArgumentException("Internal error: TypeReference constructed without actual type information");
+        }
+        type = ((ParameterizedType) superClass).getActualTypeArguments()[0];
+    }
+
+    public final Type getType() {
+        return type;
+    }
+
+    @Override
+    public final int compareTo(GenericNotation<T> o) {
+        return 0;
+    }
+}
