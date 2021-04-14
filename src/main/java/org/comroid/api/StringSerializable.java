ef8b6dca057e9032de591cf86a56ad5f1927094f
Tobias Burdow [Kaleidox] <burdoto@outlook.com>
2021-04-14 14:55:37 +0200

    add method toReader

--- src/main/java/org/comroid/api/StringSerializable.java
+++ src/main/java/org/comroid/api/StringSerializable.java
@@ -1,5 +1,11 @@
 package org.comroid.api;
 
+import java.io.StringReader;
+
 public interface StringSerializable {
     String toSerializedString();
+
+    default StringReader toReader() {
+        return new StringReader(toSerializedString());
+    }
 }
