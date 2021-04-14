ff508d20051c5564e4b527b6918858b1375611c3
Tobias Burdow [Kaleidox] <burdoto@outlook.com>
2021-04-14 20:32:50 +0200

    Update StringSerializable.java

--- src/main/java/org/comroid/api/StringSerializable.java
+++ src/main/java/org/comroid/api/StringSerializable.java
@@ -1,11 +1,12 @@
 package org.comroid.api;
 
+import java.io.Reader;
 import java.io.StringReader;
 
 public interface StringSerializable {
     String toSerializedString();
 
-    default StringReader toReader() {
+    default Reader toReader() {
         return new StringReader(toSerializedString());
     }
 }
