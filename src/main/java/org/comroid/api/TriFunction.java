78b7a3981414176670cdf4d13f187bd8fa87635f
Tobias Burdow [Kaleidox] <burdoto@outlook.com>
2021-04-21 18:34:57 +0200

    deprecated triFunction

--- src/main/java/org/comroid/api/TriFunction.java
+++ src/main/java/org/comroid/api/TriFunction.java
@@ -1,5 +1,6 @@
 package org.comroid.api;
 
-public interface TriFunction<P, T, X, R> {
+@Deprecated
+public interface TriFunction<P, T, X, R> extends NFunction.In3<P, T, X, R> {
     R apply(P p1, T p2, X p3);
 }
