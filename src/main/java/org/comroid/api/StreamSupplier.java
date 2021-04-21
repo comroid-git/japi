edb8a34ba0924af2e606e391c06200871e78a233
Tobias Burdow [Kaleidox] <burdoto@outlook.com>
2021-04-21 18:54:04 +0200

    add empty() and ofNullable()

--- src/main/java/org/comroid/api/StreamSupplier.java
+++ src/main/java/org/comroid/api/StreamSupplier.java
@@ -1,12 +1,26 @@
 package org.comroid.api;
 
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Arrays;
 import java.util.Objects;
 import java.util.stream.Stream;
 
 public interface StreamSupplier<T> extends Upgradeable<StreamSupplier<T>> {
+    static <T> StreamSupplier<T> empty() {
+        return Stream::empty;
+    }
+
     @SafeVarargs
     static <T> StreamSupplier<T> of(final T... values) {
         return () -> Stream.of(values);
+    }
+
+    @SafeVarargs
+    static <T> StreamSupplier<T> ofNullable(final T @Nullable ... values) {
+        if (values == null)
+            return empty();
+        return () -> Arrays.stream(values).filter(Objects::nonNull);
     }
 
     static <T> StreamSupplier<T> concat(final StreamSupplier<? extends T> first, final StreamSupplier<? extends T> second) {
