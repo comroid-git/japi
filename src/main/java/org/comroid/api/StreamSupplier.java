7f8bf953c2584b9c59ce0a0dde54f4b8186b2c6d
Tobias Burdow [Kaleidox] <burdoto@outlook.com>
2021-04-21 17:55:34 +0200

    Create StreamSupplier.java

---
+++ src/main/java/org/comroid/api/StreamSupplier.java
@@ -0,0 +1,24 @@
+package org.comroid.api;
+
+import java.util.Objects;
+import java.util.stream.Stream;
+
+public interface StreamSupplier<T> {
+    @SafeVarargs
+    static <T> StreamSupplier<T> of(final T... values) {
+        return () -> Stream.of(values);
+    }
+
+    static <T> StreamSupplier<T> concat(final StreamSupplier<? extends T> first, final StreamSupplier<? extends T> second) {
+        Objects.requireNonNull(first, "First Supplier");
+        Objects.requireNonNull(second, "Second Supplier");
+
+        return () -> Stream.concat(first.stream(), second.stream());
+    }
+
+    Stream<T> stream();
+
+    default StreamSupplier<T> append(final StreamSupplier<? extends T> other) {
+        return concat(this, other);
+    }
+}
