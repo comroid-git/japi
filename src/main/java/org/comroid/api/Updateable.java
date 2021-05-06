c56c6a6669c0ab94bb8d9ab511fa2f52443ac754
Tobias Burdow [Kaleidox] <burdoto@outlook.com>
2021-05-06 14:56:22 +0200

    add apistatus annotation

--- src/main/java/org/comroid/api/Updateable.java
+++ src/main/java/org/comroid/api/Updateable.java
@@ -1,7 +1,10 @@
 package org.comroid.api;
 
+import org.jetbrains.annotations.ApiStatus.Experimental;
+
 import java.util.Set;
 
+@Experimental
 public interface Updateable<P> {
     <R extends Named & ValueBox> Set<? extends R> updateFrom(P param);
 }
