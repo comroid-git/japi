96990419156a456b98a627ba28d15eed46f52f0c
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2021-05-24 20:23:41 +0200

    Update ThrowingSupplier.java

--- src/main/java/org/comroid/api/ThrowingSupplier.java
+++ src/main/java/org/comroid/api/ThrowingSupplier.java
@@ -1,5 +1,25 @@
 package org.comroid.api;
 
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.Function;
+import java.util.function.Supplier;
+
 public interface ThrowingSupplier<T, E extends Throwable> {
+    static <T, E extends Throwable> Supplier<T> rethrowing(
+            ThrowingSupplier<T, E> supplier,
+            @Nullable Function<Throwable, ? extends RuntimeException> remapper
+    ) {
+        return () -> {
+            try {
+                return supplier.get();
+            } catch (Throwable error) {
+                if (remapper != null)
+                    throw remapper.apply(error);
+                return null;
+            }
+        };
+    }
+
     T get() throws E;
 }
