2c673486a112c3fcc7f690bc88ae72a707a4df9a
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2021-06-08 04:33:52 +0200

    Create ValueProvider.java

---
+++ src/main/java/org/comroid/api/ValueProvider.java
@@ -0,0 +1,16 @@
+package org.comroid.api;
+
+import java.util.function.Function;
+
+public interface ValueProvider<I, O> extends Function<I, O> {
+    O get(I param);
+
+    default Rewrapper<O> wrap(final I param) {
+        return () -> get(param);
+    }
+
+    @Override
+    default O apply(I i) {
+        return get(i);
+    }
+}
