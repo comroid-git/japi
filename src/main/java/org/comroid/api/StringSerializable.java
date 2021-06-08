ab19fcd1d0691a53fc3ff17a6b719632210de631
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2021-06-08 04:33:14 +0200

    implement ValueBox<String>

--- src/main/java/org/comroid/api/StringSerializable.java
+++ src/main/java/org/comroid/api/StringSerializable.java
@@ -1,10 +1,22 @@
 package org.comroid.api;
 
+import org.comroid.util.StandardValueType;
+
 import java.io.Reader;
 import java.io.StringReader;
 
-public interface StringSerializable extends Readable {
+public interface StringSerializable extends Readable, ValueBox<String> {
     String toSerializedString();
+
+    @Override
+    default String getValue() {
+        return toSerializedString();
+    }
+
+    @Override
+    default ValueType<? extends String> getHeldType() {
+        return StandardValueType.STRING;
+    }
 
     @Override
     default Reader toReader() {
