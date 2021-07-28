fd651016e54e4c4bbf3d37fd64a25e3c68e98b74
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2021-07-28 10:47:30 +0200

    Create BlankException.java

---
+++ src/main/java/org/comroid/exception/BlankException.java
@@ -0,0 +1,30 @@
+package org.comroid.exception;
+
+import org.comroid.api.Named;
+
+public class BlankException extends RuntimeException implements Named {
+    private final String name;
+
+    public BlankException(String name, String format, Object... args) {
+        this(name, String.format(format, args));
+    }
+
+    public BlankException(String name, String message) {
+        super(message);
+        this.name = name;
+    }
+
+    public BlankException(String name, Throwable cause, String format, Object... args) {
+        this(name, String.format(format, args), cause);
+    }
+
+    public BlankException(String name, String message, Throwable cause) {
+        super(message, cause);
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s", name, getMessage());
+    }
+}
