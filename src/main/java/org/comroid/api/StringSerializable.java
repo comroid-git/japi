b77c7577265a9c50d211ff3c48b720e176cee7dc
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2021-08-26 11:30:18 +0200

    implement java.io.Serializable

--- src/main/java/org/comroid/api/StringSerializable.java
+++ src/main/java/org/comroid/api/StringSerializable.java
@@ -1,9 +1,10 @@
 package org.comroid.api;
 
 import java.io.Reader;
+import java.io.Serializable;
 import java.io.StringReader;
 
-public interface StringSerializable extends Readable {
+public interface StringSerializable extends Readable, Serializable {
     String toSerializedString();
 
     @Override
