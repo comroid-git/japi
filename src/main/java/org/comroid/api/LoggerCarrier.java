4eebbbaa7eec25ff476cecdba3ec73b474304166
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2022-12-24 12:33:25 +0100

    include log4j too

--- src/main/java/org/comroid/api/LoggerCarrier.java
+++ src/main/java/org/comroid/api/LoggerCarrier.java
@@ -1,5 +1,7 @@
 package org.comroid.api;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.ApiStatus.OverrideOnly;
 
 public interface LoggerCarrier {
