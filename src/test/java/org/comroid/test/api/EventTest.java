641d881cdb1bfeb1dbb3dbd61781c7b2c3ef4390
burdoto <kaleidox@comroid.org>
2023-05-20 01:15:05 +0200

    Update EventTest.java

--- src/test/java/org/comroid/test/api/EventTest.java
+++ src/test/java/org/comroid/test/api/EventTest.java
@@ -11,9 +11,9 @@ public class EventTest {
         var busB = new Event.Bus<Object>(busA);
         var busC = busB.map(Object::toString).map(StandardValueType.INTEGER::parse);
 
-        busA.listen(it->it.ifPresent(e -> System.out.println("BusA had data: "+e)));
-        busB.listen(CharSequence.class, seq-> System.out.println("BusB had char sequence: "+seq));
-        busC.listen(e->e.getData()>400, x->System.out.println("BusC was larger than 400: "+x));
+        busA.listen(e -> System.out.println("Bus A had data: "+e));
+        busB.listen(CharSequence.class, e-> System.out.println("Bus B had char sequence: "+e));
+        busC.listen(e->e.getData()>400, e->System.out.println("Bus C was larger than 400: "+e));
 
         busA.publish("123");
         busB.publish(420);
