627cd7689f21c818bddba21082e8d7e5af25219b
burdoto <kaleidox@comroid.org>
2023-05-23 11:02:48 +0200

    Update Scratch.java

--- src/test/java/org/comroid/test/Scratch.java
+++ src/test/java/org/comroid/test/Scratch.java
@@ -1,6 +1,15 @@
 package org.comroid.test;
 
+import org.comroid.api.DelegateStream;
+import org.comroid.api.Event;
+
 public class Scratch {
     public static void main(String[] args) {
+        var stdio = new Event.Bus<String>();
+        var io = new DelegateStream.IO().redirectToEventBus(stdio);
+
+        stdio.publish("no print :(");
+        stdio.publish(DelegateStream.IO.EventKey_Input, "hello world");
+        stdio.publish(DelegateStream.IO.EventKey_Error, "hello error");
     }
 }
