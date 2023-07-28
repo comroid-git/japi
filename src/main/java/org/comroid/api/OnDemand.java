3acfd490143b2c5335d399b23d9eb61d1fcfe9d5
burdoto <kaleidox@comroid.org>
2023-07-28 14:37:48 +0200

    add ctor

--- src/main/java/org/comroid/api/OnDemand.java
+++ src/main/java/org/comroid/api/OnDemand.java
@@ -12,12 +12,18 @@ import java.util.function.Supplier;
 
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public final class OnDemand<T> extends Container.Base implements Provider<T> {
-    CompletableFuture<T> delegate = new CompletableFuture<>();
+    CompletableFuture<T> delegate;
     @Getter Supplier<T> supplier;
     @Getter @Setter @NonFinal @Nullable T override;
 
     public OnDemand(Supplier<T> supplier) {
         this.supplier = supplier;
+        this.delegate = new CompletableFuture<>();
+    }
+
+    public OnDemand(CompletableFuture<T> delegate) {
+        this.delegate = delegate;
+        this.supplier = delegate::join;
     }
 
     @Override
