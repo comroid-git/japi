0caa34b21551deae3c51a9110a1ed81824ff49a3
burdoto <kaleidox@comroid.org>
2023-08-05 21:28:35 +0200

    Update PathUtil.java

--- src/main/java/org/comroid/util/PathUtil.java
+++ src/main/java/org/comroid/util/PathUtil.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 public class PathUtil {
     public static Optional<File> findExec(String name) {
         return Arrays.stream(System.getenv("PATH").split(File.pathSeparator))
-                .map(p-> Path.of(p,"java"+(OS.isWindows?".exe":"")).toFile())
+                .map(p-> Path.of(p,name+(OS.isWindows?".exe":"")).toFile())
                 .filter(File::exists)
                 .findAny();
     }
