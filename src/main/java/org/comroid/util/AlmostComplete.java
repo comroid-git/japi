5939aac07ed26f012360bd7f54f1c8d4b0dd03b7
burdoto <kaleidox@comroid.org>
2023-08-27 19:43:04 +0200

    Create AlmostComplete.java

---
+++ src/main/java/org/comroid/util/AlmostComplete.java
@@ -0,0 +1,33 @@
+package org.comroid.util;
+
+import lombok.Data;
+import org.comroid.api.Rewrapper;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+@Data
+public class AlmostComplete<T> implements Rewrapper<T> {
+    private final @NotNull Supplier<T> origin;
+    private final @Nullable Consumer<T> finalize;
+
+    public T complete(@Nullable Consumer<T> modifier) {
+        var it = origin.get();
+        if (modifier != null)
+            modifier.accept(it);
+        if (finalize != null)
+            finalize.accept(it);
+        return it;
+    }
+
+    @Override
+    public final @Nullable T get() {
+        return complete(null);
+    }
+
+    public static <T> AlmostComplete<T> of(final @NotNull T value) {
+        return new AlmostComplete<>(()->value, null);
+    }
+}
