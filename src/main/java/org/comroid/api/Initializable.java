2bb8f4e340f6a1c4dc1847be5377bded0c8820b8
burdoto <kaleidox@comroid.org>
2023-09-03 10:06:18 +0200

    add InitFailed exception

--- src/main/java/org/comroid/api/Initializable.java
+++ src/main/java/org/comroid/api/Initializable.java
@@ -1,6 +1,11 @@
 package org.comroid.api;
 
+import lombok.experimental.StandardException;
+
 public interface Initializable {
     default void initialize() throws Throwable {
     }
+
+    @StandardException
+    class InitFailed extends RuntimeException {}
 }
