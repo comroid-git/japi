c9e6cf949336a1229ef7c7f700c813f322552bc8
burdoto <kaleidox@comroid.org>
2023-09-10 07:27:38 +0200

    Update Token.java

--- src/main/java/org/comroid/util/Token.java
+++ src/main/java/org/comroid/util/Token.java
@@ -9,13 +9,11 @@ public class Token {
         var str = new StringBuilder();
         for (int i = 0; i < length; i++) {
             char c = 0;
-            byte[] b = new byte[1];
             while (!Character.isDigit(c) && !Character.isLetter(c)){
                 c=(char)(rng.nextInt(26)+'A');
             }
             str.append(c);
         }
-        //if (base64) str = new StringBuilder(Base64.encode(str.toString()));
-        return str.toString();
+        return base64 ? Base64.encode(str.toString()) : str.toString();
     }
 }
