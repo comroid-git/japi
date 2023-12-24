d1000832ce26f444c2a808a624d2c74c135692c8
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2023-12-25 00:59:53 +0100

    Update Log.java

--- src/main/java/org/comroid/api/info/Log.java
+++ src/main/java/org/comroid/api/info/Log.java
@@ -1,11 +1,13 @@
 package org.comroid.api.info;
 
+import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
 import org.comroid.util.StackTraceUtils;
 
+import java.util.Arrays;
 import java.util.logging.Level;
-import java.util.logging.LogManager;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 @UtilityClass
 public final class Log {
@@ -24,8 +26,18 @@ public final class Log {
     public static void at(Level level, String message, Throwable t) {
         getForCaller(1).log(level, message, t);
     }
-        public static void at(Level level, String message) {
+
+    public static void at(Level level, String message) {
         getForCaller(1).log(level, message);
+    }
+
+    @SneakyThrows
+    public static void sOutMP(Level level, Object... parameter) {
+        final var e = new Throwable().getStackTrace()[1];
+        get(Class.forName(e.getClassName())).log(level, "%s.%s(%s)".formatted(e.getClassName(), e.getMethodName(),
+                Arrays.stream(parameter)
+                        .map(String::valueOf)
+                        .collect(Collectors.joining(", "))));
     }
 
     private static Logger getForCaller(int skip) {
