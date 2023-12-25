26716787c3ce29dca8e185365f4c952b86fb290f
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2023-12-25 01:00:41 +0100

    Update Convertible.java

--- src/main/java/org/comroid/api/Convertible.java
+++ src/main/java/org/comroid/api/Convertible.java
@@ -1,28 +1,39 @@
 package org.comroid.api;
 
+import org.comroid.abstr.DataNode;
+import org.comroid.annotations.Annotations;
 import org.comroid.annotations.Convert;
+import org.comroid.util.Constraint;
 import org.jetbrains.annotations.ApiStatus.Experimental;
 import org.jetbrains.annotations.NotNull;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Modifier;
-import java.util.Arrays;
 import java.util.NoSuchElementException;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 @Experimental
 public interface Convertible {
-    @SuppressWarnings("unchecked")
     @Experimental
     default <R> @NotNull R convert(Class<? super R> target) {
-        final var it = this instanceof Supplier ? ((Supplier<?>) this).get() : this;
+        return convert(this, target);
+    }
+
+    @Experimental
+    @SuppressWarnings("unchecked")
+    static <R> @NotNull R convert(Object it_, Class<? super R> target) {
+        var it = it_ instanceof Supplier && !Annotations.ignore(it_.getClass(), Convertible.class)
+                ? ((Supplier<?>) it_).get() : it_;
+        Constraint.notNull(it, "source object")
+                .setHint("check implementation")
+                .run();
         if (target.isInstance(it))
             return (R) target.cast(it);
         return Stream.concat(Stream.of(target.getConstructors()),
                         Stream.of(it.getClass(), target)
                                 .flatMap(tgt -> Stream.of(tgt.getMethods())
-                                        .filter(mtd -> target.isAssignableFrom(mtd.getReturnType()))))
+                                        .filter(mtd -> target.isAssignableFrom(mtd.getReturnType())))
+                                .filter(mtd -> Modifier.isStatic(mtd.getModifiers())))
                 .filter(exe -> exe.isAnnotationPresent(Convert.class) || exe.getName().equals("upgrade"))
                 .filter(exe -> exe.getParameterCount() == 0
                         || exe.getParameterCount() == 1 && exe.getParameterTypes()[0].isInstance(it))
@@ -30,7 +41,7 @@ public interface Convertible {
                 .map(Invocable::ofExecutable)
                 .map(task -> {
                     try {
-                        return (R) task.invoke(this, this);
+                        return (R) task.invoke(it, it);
                     } catch (IllegalAccessException e) {
                         // semantically unreachable; thus we throw an AssertionError
                         throw new AssertionError("Cannot access Upgrade method " + task, e);
