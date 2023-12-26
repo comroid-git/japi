6e21e2303372007f3c38b2635f86703f81f9dc99
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2023-12-26 01:05:38 +0100

    add entry to map collector

--- src/main/java/org/comroid/util/Streams.java
+++ src/main/java/org/comroid/util/Streams.java
@@ -1,10 +1,18 @@
 package org.comroid.util;
 
+import lombok.Value;
 import lombok.experimental.UtilityClass;
+import org.comroid.api.N;
+import org.comroid.api.Named;
+import org.comroid.api.Polyfill;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.util.*;
+import java.util.AbstractMap.SimpleImmutableEntry;
+import java.util.Map.Entry;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.*;
@@ -17,8 +25,8 @@ import static java.util.stream.Stream.empty;
 
 @UtilityClass
 public class Streams {
-    public static <T> Stream<T> of(Iterator<T> iterator,int size) {
-        return of(Spliterators.spliterator(iterator,size,0));
+    public static <T> Stream<T> of(Iterator<T> iterator, int size) {
+        return of(Spliterators.spliterator(iterator, size, 0));
     }
 
     public static <T> Stream<T> of(Iterable<T> iterable) {
@@ -94,6 +102,186 @@ public class Streams {
                 .get();
     }
 
+    @UtilityClass
+    public class Multi {
+        //region main methods
+        @WrapWith("map")
+        public <A, B> Function<A, Entry<A, B>> explode(final @NotNull Function<A, B> function) {
+            return t -> new SimpleImmutableEntry<>(t, function.apply(t));
+        }
+
+        @WrapWith("map")
+        public <A, B, X> Function<Entry<A, B>, Entry<X, B>> crossToA(final @NotNull BiFunction<A, B, X> function) {
+            return cross(function, (a, b) -> b);
+        }
+
+        @WrapWith("map")
+        public <A, B, Y> Function<Entry<A, B>, Entry<A, Y>> crossToB(final @NotNull BiFunction<A, B, Y> function) {
+            return cross((a, b) -> a, function);
+        }
+
+        @WrapWith("map")
+        public <A, B, X, Y> Function<Entry<A, B>, Entry<X, Y>> cross(
+                final @NotNull BiFunction<A, B, X> xFunction,
+                final @NotNull BiFunction<A, B, Y> yFunction) {
+            return e -> new SimpleImmutableEntry<>(
+                    xFunction.apply(e.getKey(), e.getValue()),
+                    yFunction.apply(e.getKey(), e.getValue()));
+        }
+
+        @WrapWith("map")
+        public <A, B, R> Function<Entry<A, B>, R> combine(final @NotNull BiFunction<A, B, R> function) {
+            return e -> function.apply(e.getKey(), e.getValue());
+        }
+        //endregion
+
+        //region regular stream functions
+        @WrapWith("peek")
+        public <A, B> Consumer<Entry<A, B>> peekA(final @NotNull Consumer<A> consumer) {
+            return peek(consumer, N.Consumer.nop());
+        }
+
+        @WrapWith("peek")
+        public <A, B> Consumer<Entry<A, B>> peekB(final @NotNull Consumer<B> consumer) {
+            return peek(N.Consumer.nop(), consumer);
+        }
+
+        @WrapWith("peek")
+        public <A, B> Consumer<Entry<A, B>> peek(
+                final @NotNull Consumer<A> aConsumer,
+                final @NotNull Consumer<B> bConsumer) {
+            return peek((a, b) -> {
+                aConsumer.accept(a);
+                bConsumer.accept(b);
+            });
+        }
+
+        @WrapWith("peek")
+        public <A, B> Consumer<Entry<A, B>> peek(final @NotNull BiConsumer<A, B> consumer) {
+            return e -> consumer.accept(e.getKey(), e.getValue());
+        }
+
+        @WrapWith("flatMap")
+        public <A, B> Function<Entry<A, B>, Stream<Entry<A, B>>> filterA(@NotNull Predicate<A> predicate) {
+            return filter(predicate, $ -> true);
+        }
+
+        @WrapWith("flatMap")
+        public <A, B> Function<Entry<A, B>, Stream<Entry<A, B>>> filterB(@NotNull Predicate<B> predicate) {
+            return filter($ -> true, predicate);
+        }
+
+        @WrapWith("flatMap")
+        public <A, B> Function<Entry<A, B>, Stream<Entry<A, B>>> filter(
+                @NotNull Predicate<A> aPredicate,
+                @NotNull Predicate<B> bPredicate) {
+            return filter(OP.LogicalAnd, aPredicate, bPredicate);
+        }
+
+        @WrapWith("flatMap")
+        public <A, B> Function<Entry<A, B>, Stream<Entry<A, B>>> filter(
+                final @NotNull OP op,
+                final @NotNull Predicate<A> aPredicate,
+                final @NotNull Predicate<B> bPredicate) {
+            return filter((a, b) -> op.test(() -> aPredicate.test(a), () -> bPredicate.test(b)));
+        }
+
+        @WrapWith("flatMap")
+        public <A, B> Function<Entry<A, B>, Stream<Entry<A, B>>> filter(final @NotNull BiPredicate<A, B> predicate) {
+            return e -> predicate.test(e.getKey(), e.getValue()) ? Stream.of(e) : empty();
+        }
+
+        @WrapWith("map")
+        public <A, B, X> Function<Entry<A, B>, Entry<X, B>> mapA(final @NotNull Function<A, X> function) {
+            return map(function, Function.identity());
+        }
+
+        @WrapWith("map")
+        public <A, B, Y> Function<Entry<A, B>, Entry<A, Y>> mapB(final @NotNull Function<B, Y> function) {
+            return map(Function.identity(), function);
+        }
+
+        @WrapWith("map")
+        public <A, B, X, Y> Function<Entry<A, B>, Entry<X, Y>> map(
+                final @NotNull Function<A, X> axFunction,
+                final @NotNull Function<B, Y> byFunction) {
+            return e -> new SimpleImmutableEntry<>(axFunction.apply(e.getKey()), byFunction.apply(e.getValue()));
+        }
+
+        @WrapWith("flatMap")
+        public <A, B, X> Function<Entry<A, B>, Stream<Entry<X, B>>> flatMapA(final @NotNull Function<A, Stream<X>> function) {
+            return flatMap(Adapter.sideA(), function);
+        }
+
+        @WrapWith("flatMap")
+        public <A, B, Y> Function<Entry<A, B>, Stream<Entry<A, Y>>> flatMapB(final @NotNull Function<B, Stream<Y>> function) {
+            return flatMap(Adapter.sideB(), function);
+        }
+
+        @WrapWith("flatMap")
+        public <A, B, X, Y, I, O> Function<Entry<A, B>, Stream<Entry<X, Y>>> flatMap(
+                final @NotNull Adapter<A, B, X, Y, I, O> adapter,
+                final @NotNull Function<I, Stream<O>> function) {
+            return e -> function.apply(adapter.input.apply(e)).map(o -> adapter.merge(e, o));
+        }
+
+        @WrapWith("forEach")
+        public <A, B> Consumer<Entry<A, B>> forEach(
+                final @NotNull Consumer<A> aConsumer,
+                final @NotNull Consumer<B> bConsumer) {
+            return forEach((a, b) -> {
+                aConsumer.accept(a);
+                bConsumer.accept(b);
+            });
+        }
+
+        @WrapWith("forEach")
+        public <A, B> Consumer<Entry<A, B>> forEach(final @NotNull BiConsumer<A, B> consumer) {
+            return e -> consumer.accept(e.getKey(), e.getValue());
+        }
+
+        @WrapWith("collect")
+        public <K, V> Collector<Entry<K, V>, Map<K, V>, Map<K, V>> collector() {
+            return collector(HashMap::new);
+        }
+
+        @WrapWith("collect")
+        public <K, V> Collector<Entry<K, V>, Map<K, V>, Map<K, V>> collector(final Supplier<Map<K, V>> mapSupplier) {
+            return Collector.of(mapSupplier,
+                    (m, e) -> m.put(e.getKey(), e.getValue()),
+                    (a, b) -> {
+                        a.putAll(b);
+                        return a;
+                    },
+                    Collections::unmodifiableMap);
+        }
+        //endregion
+
+        @Value
+        public static class Adapter<A, B, X, Y, I, O> {
+            Function<Entry<A, B>, I> input;
+            BiFunction<Entry<A, B>, O, X> outputX;
+            BiFunction<Entry<A, B>, O, Y> outputY;
+
+            public static <A, B, X> Adapter<A, B, X, B, A, X> sideA() {
+                return new Adapter<>(Entry::getKey, (e, x) -> x, (e, y) -> e.getValue());
+            }
+
+            public static <A, B, Y> Adapter<A, B, A, Y, B, Y> sideB() {
+                return new Adapter<>(Entry::getValue, (e, y) -> e.getKey(), (e, y) -> y);
+            }
+
+            private Entry<X, Y> merge(Entry<A, B> entry, O output) {
+                return new SimpleImmutableEntry<>(outputX.apply(entry, output), outputY.apply(entry, output));
+            }
+        }
+
+        @Retention(RetentionPolicy.CLASS)
+        private @interface WrapWith {
+            String value();
+        }
+    }
+
     public enum Strategy {
         Every,
         Opposite,
@@ -127,5 +315,41 @@ public class Streams {
                 return apply(predicate.test(t));
             }
         }
+    }
+
+    public enum OP implements BiPredicate<BooleanSupplier, BooleanSupplier>, Named {
+        LogicalAnd {
+            @Override
+            public boolean test(BooleanSupplier l, BooleanSupplier r) {
+                return l.getAsBoolean() && r.getAsBoolean();
+            }
+        },
+        LogicalOr {
+            @Override
+            public boolean test(BooleanSupplier l, BooleanSupplier r) {
+                return l.getAsBoolean() || r.getAsBoolean();
+            }
+        },
+        LogicalXor {
+            @Override
+            public boolean test(BooleanSupplier l, BooleanSupplier r) {
+                return l.getAsBoolean() ^ r.getAsBoolean();
+            }
+        },
+        BitwiseAnd {
+            @Override
+            public boolean test(BooleanSupplier l, BooleanSupplier r) {
+                return l.getAsBoolean() & r.getAsBoolean();
+            }
+        },
+        BitwiseOr {
+            @Override
+            public boolean test(BooleanSupplier l, BooleanSupplier r) {
+                return l.getAsBoolean() | r.getAsBoolean();
+            }
+        };
+
+        @Override
+        public abstract boolean test(BooleanSupplier l, BooleanSupplier r);
     }
 }
--- src/test/java/org/comroid/test/util/StreamExtTest.java
+++ src/test/java/org/comroid/test/util/StreamExtTest.java
@@ -5,6 +5,10 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static org.comroid.util.Streams.Multi.*;
+import static org.comroid.util.Streams.Multi.Adapter.*;
 
 public class StreamExtTest {
     @Test
@@ -14,5 +18,14 @@ public class StreamExtTest {
         Assert.assertEquals("Batch count", 5, list.size());
         for (var batch : list)
             Assert.assertEquals("Batch length", 50, batch.size());
+    }
+
+    @Test
+    public void testEntries() {
+        Stream.of("mushroom", "doretta", "rock and stone!")
+                .map(explode(String::length))
+                .flatMap(flatMap(sideA(), s->s.chars().boxed()))
+                .map(combine((s,l)->s+l))
+                .forEach(System.out::println);
     }
 }
