69ddb4f8dbabdf9a73f4da0604b80cfc24667c93
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2024-05-21 21:04:32 +0200

    fix entity service lifecycle

--- src/main/java/org/comroid/api/tree/Terminatable.java
+++ src/main/java/org/comroid/api/tree/Terminatable.java
@@ -2,5 +2,7 @@ package org.comroid.api.tree;
 
 public interface Terminatable {
     default void terminate() throws Throwable {
+        if (this instanceof AutoCloseable ac)
+            ac.close();
     }
 }
