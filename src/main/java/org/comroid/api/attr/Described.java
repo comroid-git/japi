e4bbd09e95a122c03ce4dea7ef43e2034eee00e1
Kaley <kaleidox@comroid.org>
2024-07-13 11:54:48 +0200

    add filter vs blank description

--- src/main/java/org/comroid/api/attr/Described.java
+++ src/main/java/org/comroid/api/attr/Described.java
@@ -3,10 +3,13 @@ package org.comroid.api.attr;
 import org.comroid.annotations.AnnotatedTarget;
 import org.comroid.annotations.internal.Annotations;
 
+import static java.util.function.Predicate.not;
+
 public interface Described extends AnnotatedTarget.Extension {
     default String getDescription() {
         return element()
                 .map(Annotations::descriptionText)
+                .filter(not(String::isBlank))
                 .orElse("No description");
     }
 }
