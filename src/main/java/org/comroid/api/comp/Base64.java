d87b550d89f051476a1373ab8d0d6dc5cda3a9e6
Kaley <kaleidox@comroid.org>
2024-12-22 18:09:10 +0100

    introduce MessageWrapper to all SubMods

--- src/main/java/org/comroid/api/comp/Base64.java
+++ src/main/java/org/comroid/api/comp/Base64.java
@@ -1,5 +1,8 @@
 package org.comroid.api.comp;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 
 import static java.util.Base64.*;
