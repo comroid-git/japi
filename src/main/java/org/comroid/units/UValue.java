f79ce97ef1a8b1d142d975000fd4b67ae69fd870
burdo <contact@kaleidox.de>
2025-10-22 22:28:10 +0200

    Update unit parsing

--- src/main/java/org/comroid/units/UValue.java
+++ src/main/java/org/comroid/units/UValue.java
@@ -2,6 +2,9 @@ package org.comroid.units;
 
 import lombok.Builder;
 import lombok.Value;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.regex.Pattern;
 
 import static org.jetbrains.annotations.ApiStatus.*;
 
@@ -9,8 +12,17 @@ import static org.jetbrains.annotations.ApiStatus.*;
 @Builder
 @Experimental
 public class UValue extends Number {
+    public static final Pattern PATTERN = Pattern.compile("(?<value>-?[0-9]+(\\.[0-9]+)?)(?<unit>.+)?");
+
+    public static UValue parse(String str) {
+        var matcher = PATTERN.matcher(str);
+        if (!matcher.matches()) throw new IllegalArgumentException("Cannot parse UnitValue");
+        var unit = matcher.group("unit");
+        return new UValue(Double.parseDouble(matcher.group("value")), unit == null ? null : new Unit(unit));
+    }
+
     double value;
-    Unit   unit;
+    @Nullable Unit unit;
 
     @Override
     public int intValue() {
--- src/main/java/org/comroid/units/Unit.java
+++ src/main/java/org/comroid/units/Unit.java
@@ -1,13 +1,6 @@
 package org.comroid.units;
 
-import lombok.Builder;
-import lombok.Value;
-
 import static org.jetbrains.annotations.ApiStatus.*;
 
-@Value
-@Builder
 @Experimental
-public class Unit {
-    String name;
-}
+public record Unit(String name) {}
