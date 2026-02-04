644fbb3bd8ee91a38cb740bd4047cec03986e913
burdo <contact@kaleidox.de>
2026-02-04 21:35:18 +0100

    introduce rankinfo command

--- src/main/java/org/comroid/api/net/luckperms/model/group/GroupData.java
+++ src/main/java/org/comroid/api/net/luckperms/model/group/GroupData.java
@@ -4,6 +4,12 @@ import org.comroid.api.net.luckperms.model.dto.Metadata;
 import org.comroid.api.net.luckperms.model.node.Node;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Objects;
+
 public record GroupData(
         String name, @Nullable String displayName, int weight, Node[] nodes, @Nullable Metadata metadata
-) {}
+) {
+    public String bestName() {
+        return Objects.requireNonNullElse(displayName, name);
+    }
+}
