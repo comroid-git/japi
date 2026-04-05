b125614312d51205c46fdd3ea7eb12dca8ee28c7
Kaleidox <git@kaleidox.de>
2026-04-05 12:24:00 +0200

    remove redundant methods

--- src/main/java/org/comroid/interaction/node/model/ContextManipulatingNode.java
+++ src/main/java/org/comroid/interaction/node/model/ContextManipulatingNode.java
@@ -5,18 +5,13 @@ import org.comroid.interaction.annotation.ContextFilter;
 import org.comroid.interaction.annotation.Interaction;
 
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.stream.Stream;
 
 public interface ContextManipulatingNode {
     Interaction.Resolved getInteraction();
 
-    Collection<ContextFilter> getFilters();
-
-    Collection<ContextDefinition> getDefinitions();
-
     default Stream<ContextFilter> getFilter(String key) {
-        return getFilters().stream().filter(it -> it.filter().startsWith(key));
+        return Arrays.stream(getInteraction().filter()).filter(it -> it.filter().startsWith(key));
     }
 
     default Stream<String> getFilterValues(String key) {
@@ -24,7 +19,7 @@ public interface ContextManipulatingNode {
     }
 
     default Stream<ContextDefinition> getDefinition(String key) {
-        return getDefinitions().stream().filter(it -> it.value().startsWith(key));
+        return Arrays.stream(getInteraction().definitions()).filter(it -> it.value().startsWith(key));
     }
 
     default Stream<String> getDefinitionValues(String key) {
